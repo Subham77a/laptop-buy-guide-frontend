ca84358104bad71010fe1c8010cfb6ceb4e83282
subham <subham.mcabhu24@gmail.com>
2025-12-30 17:42:15 +0530

    final 2

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
         <div className="profile-wrapper">
           <div className="profile-glow"></div>
           <img
-            src="../public/images/profilePic.jpg"
+            src="../images/profilePic.jpg"
             alt="Profile"
             className="profile-image"
           />
